30dad275973fc73653dc0737ceb7240ac137ec14
Martin Schmid <m.schmid@equitania.de>
2025-04-06 11:07:30 +0200

    [UPDATE] Bump version to 1.3.3 and add cache path replacement function
    - Updated version in __init__.py to 1.3.3
    - Added function to replace cache path in Nginx templates based on service name

--- nginx_set_conf/__init__.py
+++ nginx_set_conf/__init__.py
@@ -2,6 +2,13 @@
 nginx-set-conf - Ein Werkzeug zur Verwaltung von Nginx-Konfigurationen
 """
 
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 
 from . import config_templates, utils
+
+def replace_cache_path(template, service_name):
+    """Replace the cache path in a template with a unique path based on service name."""
+    return template.replace(
+        'proxy_cache_path /tmp', 
+        f'proxy_cache_path /var/cache/nginx/{service_name}'
+    )
